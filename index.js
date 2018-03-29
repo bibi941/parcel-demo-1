5522780a020b44c1f7eebba5e47c872be4dbe223
finley <sssrrr@vip.qq.com>
2018-03-29 16:28:02 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import x from './module-1'
+import y from './module-2'
+import './main.css'
+console.log('hello world')
+x()
+y()
---
+++ module-1.js
@@ -0,0 +1,5 @@
+export default function () { 
+    console.log('fuck webpack');
+    let a = 3
+    
+ }
